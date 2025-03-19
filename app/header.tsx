12eea2935d60a03af9a498cd4e0ebd892922fa61
Alex Arvanitidis <alex_arvanitidis@mail.ntua.gr>
2025-03-19 15:39:44 +0200

    feat: add image on header

--- app/header.tsx
+++ app/header.tsx
@@ -1,23 +1,37 @@
 'use client'
 import { TextEffect } from '@/components/ui/text-effect'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export function Header() {
   return (
-    <header className="mb-8 flex items-center justify-between">
-      <div>
-        <Link href="/" className="font-medium text-black dark:text-white">
-          Alex Arvanitidis
+    <header className="mb-8 flex justify-between">
+      <div className="flex w-full flex-row items-center gap-5">
+        <Link href="/">
+          <div className="z-10 cursor-pointer" draggable="false">
+            <Image
+              alt="Photo of Alex Arvanitidis"
+              width={72}
+              height={72}
+              src="/alex-arvanitidis.jpg"
+              className="rounded-full"
+            />
+          </div>
         </Link>
-        <TextEffect
-          as="p"
-          preset="fade"
-          per="char"
-          className="text-zinc-600 dark:text-zinc-500"
-          delay={0.5}
-        >
-          Machine Learning Engineer
-        </TextEffect>
+        <div className="flex flex-col justify-between gap-1 tracking-wide">
+          <Link href="/" className="font-medium text-black dark:text-white">
+            Alex Arvanitidis
+          </Link>
+          <TextEffect
+            as="p"
+            preset="fade"
+            per="char"
+            className="text-zinc-600 dark:text-zinc-500"
+            delay={0.5}
+          >
+            Machine Learning Engineer
+          </TextEffect>
+        </div>
       </div>
     </header>
   )
