863d06fe683e4fc0fbc1e1a4b604f66d4fb36800
Alex Arvanitidis <alex_arvanitidis@mail.ntua.gr>
2025-04-03 11:11:11 +0300

    fix: improve opengraph data

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,14 @@
 import { AnimatedBackground } from '@/components/ui/animated-background'
 import { BLOG_POSTS } from '@/app/data'
 import Link from 'next/link'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title:
+    'Blog | Alex Arvanitidis - Machine Learning Engineer & Full-Stack engineer',
+  description:
+    'Personal blog by Alex Arvanitidis, sharing insights on machine learning, software engineering, web development, and personal growth. Follow along for tutorials, thoughts on tech trends, and behind-the-scenes on building Jaqpot and other projects.',
+}
 
 export default function Page() {
   return (
