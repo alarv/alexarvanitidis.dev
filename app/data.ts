d376a29de4df9794ee1753db5ba9576bbd424aeb
Alex Arvanitidis <alex_arvanitidis@mail.ntua.gr>
2025-05-28 12:02:44 +0300

    feat: blogpost on ideas marinate

--- app/data.ts
+++ app/data.ts
@@ -133,6 +133,13 @@ export const WORK_EXPERIENCE: WorkExperience[] = [
 
 export const BLOG_POSTS: BlogPost[] = [
   {
+    title: 'The luxury of letting ideas marinate',
+    description:
+      'This post explores the power of letting ideas sit. It reflects on how having time, space, and a clear mind can lead to better solutions — especially in engineering and research. It contrasts rushed patches with well-thought-out fixes that emerge when pressure is low and the mind is calm.',
+    link: '/blog/the-luxury-of-letting-ideas-marinate',
+    uid: 'blog/the-luxury-of-letting-ideas-marinate',
+  },
+  {
     title: 'I asked AI about the future of AI (Yes, AI-ception)',
     description:
       'I wanted to know: where is AI going in the next 10+ years? Is diving deeper into large language models (LLMs) in 2025 still a good bet for developers like me? What jobs will rise? What skills will matter?',
